2f831060483851142bc98a803d2c3259f0bd6a1d
Al1erEgo <al1erego@gmail.com>
2023-08-01 15:16:52 +0200

    feat: refactor structure feat: add splashscreen and image for the first screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
+import * as SplashScreen from 'expo-splash-screen'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'terminal-dosis': require('./src/assets/terminal-dosis.ttf')
+  })
+
+  useEffect(()=>{
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync()
+    }
+    prepare()
+  }, [])
+
+  if (!fontsLoaded) {
+    return undefined
+  } else {
+    SplashScreen.hideAsync()
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
